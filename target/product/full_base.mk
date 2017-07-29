fe0d8b1e2c6162a6a36e67a3ac5ac585dd946fb7
jl90 <leonardjason90.jl@gmail.com>
2017-07-29 17:44:11 +0000

    Change default ringtone.

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -35,7 +35,7 @@ PRODUCT_PACKAGES += \
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.ringtone=Zen.ogg \
+    ro.config.ringtone=Kill_Or_Become.ogg \
     ro.config.notification_sound=Chime.ogg
 
 # Put en_US first in the list, so make it default.
