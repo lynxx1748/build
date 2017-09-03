8feff1b5e5d160415e1128dd1f74018a44eecdd6
lynxx1738 <lynxx1738@gmail.com>
2017-09-03 08:04:07 -0500

    Change defualt Revision

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -43,7 +43,7 @@ DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
 custom_local_manifest = ".repo/local_manifests/unholy_manifest.xml"
-custom_default_revision = "n7"
+custom_default_revision = "n7x-caf"
 custom_dependencies = "unholy.dependencies"
 org_manifest = "UnholyDevs"  # leave empty if org is provided in manifest
 org_display = "UnholyDevs"  # needed for displaying
